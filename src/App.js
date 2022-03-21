cab2fadcf4fe0b5aa46fbd2a9ebcdb43423aff9a
Winston McHetz <winstonmchetz@R2Hs-MBP.attlocal.net>
2022-03-21 12:40:16 -0400

    added products

--- src/App.js
+++ src/App.js
@@ -5,9 +5,29 @@ function App() {
   return (
     <div className="App">
      <Product src="/products/beach-shirt1.jpg" name="beach shirt"/>
-     <Product src="/products/beach-shirt2.jpg"/>
-     <Product src="/products/black-dress-shirt.jpg"/>
-     <Product src="/products/black-dress-shirt2.jpg"/>
+     <Product src="/products/beach-shirt2.jpg" name=""/>
+     <Product src="/products/black-dress-shirt.jpg" name="black dress shirt"/>
+     <Product src="/products/black-dress-shirt2.jpg" name=""/>
+     <Product src="/products/black-jacket1.jpg" name="black jacket"/>
+     <Product src="/products/black-jacket2.jpg" name=""/>
+     <Product src="/products/blue-blazer1.jpg" name="blue blazer"/>
+     <Product src="/products/blue-blazer2.jpg" name=""/>
+     <Product src="/products/bomber-jacket1.jpg" name="bomber jacket"/>
+     <Product src="/products/bomber-jacket2.jpg" name=""/>
+     <Product src="/products/chain-blouson1.jpg" name="chain blouson"/>
+     <Product src="/products/chain-blouson2.jpg" name=""/>
+     <Product src="/products/gold-shirt1.jpg" name="gold shirt"/>
+     <Product src="/products/gold-shirt2.jpg" name=""/>
+     <Product src="/products/gray-tshirt1.jpeg" name="gray t shirt"/>
+     <Product src="/products/gray-tshirt2.jpeg" name=""/>
+     <Product src="/products/renaissance-jacket1.jpg" name="renaissance jacket"/>
+     <Product src="/products/renaissance-jacket2.jpg" name=""/>
+     <Product src="/products/stripe-shirt1.jpg" name="stripe shirt"/>
+     <Product src="/products/stripe-shirt2.jpg" name=""/>
+     <Product src="/products/white-button1.jpg" name="white button-up shirt"/>
+     <Product src="/products/white-button2.jpg" name=""/>
+     <Product src="/products/white-tshirt1.jpeg" name="white t shirt"/>
+     <Product src="/products/white-tshirt2.jpeg" name=""/>
     </div>
   );
 }
